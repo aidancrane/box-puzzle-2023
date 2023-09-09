1fa2906f8ade89fc1a7c1075c8ad933585c8f4d0
Aidan Crane <aidancrane78@gmail.com>
2023-09-10 00:27:05 +0100

    Configured vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,8 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/styles.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    watch: {
+        usePolling: true, // Enable polling for file changes
+        interval: 1000,   // Polling interval in milliseconds
+      },
+}, 
 });
